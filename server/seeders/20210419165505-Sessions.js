4208e0a2b281c69116b171d620435f0b87fbca51
ChaTbiM <chatbimdev@gmail.com>
2021-04-24 01:33:26 +0200

    return session organized by specialty and year

--- server/seeders/20210419165505-Sessions.js
+++ server/seeders/20210419165505-Sessions.js
@@ -8,11 +8,14 @@ module.exports = {
      * Add seed commands here.
      *
      * Example:
-      Classes 1st year :  A.P.P_MS ,  A.P.S_MS , ABMI_MS 
-      Classes 2nd year : A.N.C_MS , Agru_LA
-      Classes 3rd year : ABSE5_LA , 
+      Classes 1st year Licence : M211_LS: Physique 
+      Classes 2nd year Licence : BOT._LS : Botanique
+      Classes 3rd year Licence : Mi.Al_LS :	Microbiologie Alimentaire
+      Classes 1st year Masters : A.P.P_MS :  Analyses des propriétés physicochimiques des biomolécules
+      Classes 2nd year Master:  M3:MB_MS : Modification des biomolécules par voie chimique et enzymatiq , 
     */
      await queryInterface.bulkInsert('Sessions', [
+      //  M211_LS
        {
         teacher_id: 103,
         group_number: 2,
@@ -60,6 +63,58 @@ module.exports = {
         group_number: 4,
         date_time: "2021-04-25 10:45:00",
         class_id: "M211_LS"
+      },
+      // M3:MB_MS
+      {
+        teacher_id: 103,
+        group_number: 1,
+        date_time: "2021-04-28 11:45:00",
+        class_id: "M3:MB_MS"
+      },
+      {
+        teacher_id: 103,
+        group_number: 2,
+        date_time: "2021-04-28 10:45:00",
+        class_id: "M3:MB_MS"
+      },
+      // A.P.P_MS
+      {
+        teacher_id: 103,
+        group_number: 2,
+        date_time: "2021-04-26 08:30:00",
+        class_id: "A.P.P_MS"
+      },
+      {
+        teacher_id: 103,
+        group_number: 4,
+        date_time: "2021-04-27 09:45:00",
+        class_id: "A.P.P_MS"
+      },
+      // BOT._LS
+      {
+        teacher_id: 103,
+        group_number: 1,
+        date_time: "2021-04-28 08:30:00",
+        class_id: "BOT._LS"
+      },
+      {
+        teacher_id: 103,
+        group_number: 7,
+        date_time: "2021-04-28 10:30:00",
+        class_id: "BOT._LS"
+      },
+      // Mi.Al_LS
+      {
+        teacher_id: 103,
+        group_number: 4,
+        date_time: "2021-04-25 13:00:00",
+        class_id: "Mi.Al_LS"
+      },
+      {
+        teacher_id: 103,
+        group_number: 2,
+        date_time: "2021-04-26 13:00:00",
+        class_id: "Mi.Al_LS"
       },
     ], {});
   },
--- server/services/session.js
+++ server/services/session.js
@@ -3,35 +3,52 @@ const { sequelize, models } = require("../database/db");
 
 async function getAllSessionsByProfessorId(professorId) {
 
-    const sessions = await sequelize.query(`SELECT S.id AS session_id ,spécialité,S.group_number, AnnéeEtude AS AnneeEtude ,O.Code_fillière AS Code_filliere  , C.Réf_Cours, C.Cours FROM Options O 
+    const sessions = await sequelize.query(`SELECT S.id AS session_id, group_number,date_time, Année AS Annee, Nbre_Année AS Nbre_Annee , Fillière AS Specialty FROM Options O 
     INNER JOIN Cours C ON O.Code_fillière = C.FilièreEtude 
     INNER JOIN Sessions S ON S.class_id = C.Réf_Cours
-    WHERE S.teacher_id = ${professorId}
-    ORDER BY spécialité , AnnéeEtude , S.group_number
-    `, {
-        raw: true
-    });
+    WHERE S.teacher_id = ${professorId} 
+    ORDER BY group_number
+    `);
 
     let sessionsBySpeciality = {};
     sessions[0].forEach((session) => {
-        const specialityCode = 'Code_filliere';
-        const year = "AnneeEtude";
-        console.log('year', session[specialityCode]);
-        if (sessionsBySpeciality[session[specialityCode]] && sessionsBySpeciality[session[specialityCode]][session[year]]) {
-            return sessionsBySpeciality[session[specialityCode]][session[year]].push(session);
-        } else {
-            sessionsBySpeciality[session[specialityCode]] = {};
-            sessionsBySpeciality[session[specialityCode]][session[year]] = [];
+        const specialtyCode = 'Specialty';
+        const yearCode = "Annee";
+        const numberOfYears = "Nbre_Annee";
 
-            return sessionsBySpeciality[session[specialityCode]][session[year]].push(session);
+        session.year = getYearFromYearCodeAndNumberOfYears(session[yearCode], session[numberOfYears])
+
+        if(session[specialtyCode].toLowerCase().includes("tc")){
+            const tcRegex = new RegExp("TC","i")
+            session[specialtyCode] = session[specialtyCode].replace(tcRegex,"Tronc commun")  
+        }
+
+        // Construct Organized DataStructure of Sessions
+        if (sessionsBySpeciality[session[specialtyCode]]) {
+            if (sessionsBySpeciality[session[specialtyCode]][session.year]) {
+            } else {
+                sessionsBySpeciality[session[specialtyCode]][session.year] = [];
+            }
+            sessionsBySpeciality[session[specialtyCode]][session.year].push(session)
+        } else {
+            sessionsBySpeciality[session[specialtyCode]] = {};
+            sessionsBySpeciality[session[specialtyCode]][session.year] = [];
+            sessionsBySpeciality[session[specialtyCode]][session.year].push(session)
         }
     })
 
-    return sessionsBySpeciality;
-    if (sessions) {
-        return sessions;
+    if (sessionsBySpeciality) {
+        return sessionsBySpeciality;
     }
     return null;
+}
+
+function getYearFromYearCodeAndNumberOfYears(yearCode, numberOfYears) {
+    const year = Number(yearCode[0]);
+    if (numberOfYears === 2) {
+        return year + 3;
+    }
+    return year;
 }
 
 
