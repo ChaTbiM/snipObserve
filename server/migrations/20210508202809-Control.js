f7726aa2db26224af8dcc8a2fcc9631b07321dff
ChaTbiM <chatbimdev@gmail.com>
2021-05-09 03:06:40 +0200

    created control table/migration

---
+++ server/migrations/20210508202809-Control.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+     await queryInterface.createTable('Control',
+     {
+       id: {
+         type: Sequelize.INTEGER,
+         primaryKey: true,
+         autoIncrement: true,
+       },
+       session_id: Sequelize.INTEGER,
+       student_id: Sequelize.INTEGER,
+       absences_number: Sequelize.TINYINT,
+       session_evaluation: Sequelize.ENUM(1, 0),
+       test_grade: Sequelize.FLOAT
+     });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.dropTable('Control');
+  }
+};
